c5981698a085d0a244877a72007c1394e6d8ae91
naimxno <naimxno@gmail.com>
2022-04-04 17:09:55 +0600

    404 error msg

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Blogs = () => {
   return (
     <div>
-      <h1>ekto blogs poro </h1>
+      <div>
+        <h1>Q : What is context API?</h1>
+        <p><strong>Ans :</strong></p>
+      </div>
+      <div>
+        <h1>Q : What is semantic tag?</h1>
+        <p><strong>Ans :</strong></p>
+      </div>
+      <div>
+        <h1>Q : What is inline block element?</h1>
+        <p><strong>Ans :</strong></p>
+      </div>
     </div>
   );
 };
---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <img style={{ width: "100%" }} src="https://img.freepik.com/free-vector/404-error-page-found-minimalist-dark-concept-cryptocurrency-website_111925-130.jpg?w=996" alt="" />
+    </div>
+  );
+};
+
+export default Error;
