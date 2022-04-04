cba2e53ba9e4d76d802d3aaabf7237c3a174371e
naimxno <naimxno@gmail.com>
2022-04-04 07:13:47 +0600

    chart-comlete

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import { Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import "./Dashboard.css"
 
 const Dashboard = () => {
+  const data = [
+    {
+      "month": "Mar",
+      "investment": 100000,
+      "sell": 241,
+      "revenue": 10401
+    },
+    {
+      "month": "Apr",
+      "investment": 200000,
+      "sell": 423,
+      "revenue": 24500
+    },
+    {
+      "month": "May",
+      "investment": 500000,
+      "sell": 726,
+      "revenue": 67010
+    },
+    {
+      "month": "Jun",
+      "investment": 500000,
+      "sell": 529,
+      "revenue": 40405
+    },
+    {
+      "month": "Jul",
+      "investment": 600000,
+      "sell": 601,
+      "revenue": 50900
+    },
+    {
+      "month": "Aug",
+      "investment": 700000,
+      "sell": 670,
+      "revenue": 61000
+    }
+  ]
   return (
-    <div>
-      <h1>kisu chart akao</h1>
+    <div className='chart'>
+      <h1>TwoLevelPieChart</h1>
+      <LineChart width={400} height={400} data={data}>
+        <Line dataKey={"investment"}></Line>
+        <Line dataKey={"revenue"}></Line>
+        <YAxis dataKey={"investment"}></YAxis>
+        <XAxis dataKey={"month"}></XAxis>
+        <Tooltip />
+      </LineChart>
+      <h1>TinyBarChart</h1>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          dataKey="sell"
+          cx={200}
+          cy={200}
+          outerRadius={60}
+          fill="#8884d8"
+        />
+        <Pie
+          data={data}
+          dataKey="revenue"
+          cx={200}
+          cy={200}
+          innerRadius={70}
+          outerRadius={90}
+          fill="#82ca9d"
+          label
+        />
+        <Tooltip />
+      </PieChart>
     </div>
   );
 };
